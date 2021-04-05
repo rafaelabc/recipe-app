e786b4f565d22b654763cd74b6adc8935a23d956
Rafaela <rafaela.barbara.custodio@gmail.com>
2021-04-05 00:42:53 -0300

    arrumando arquivos  e pastas

--- backend/recipeApp/src/main/java/com/rafaela/recipeApp/repository/ReceitaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rafaela.recipeApp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.rafaela.recipeApp.model.Receita;
-
-public interface ReceitaRepository extends JpaRepository <Receita, Long> {
-
-}
